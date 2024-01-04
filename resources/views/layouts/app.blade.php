aae765f29b81ac36aafda4373e15b4c053c5adae
Rodrigo Suarez <ing.rsuarez@gmail.com>
2024-01-04 14:28:06 -0300

    Add app csrf on livewire home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
      {{-- <meta http-equiv="refresh" content="30"> --}}
     <!-- CSRF Token -->
-    {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script> --}}
     <title>{{ config('app.name', 'ADMESYS') }}</title>
 
