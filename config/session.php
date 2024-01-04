c35a364dcf8803ad1067878889e90a711f414f00
Rodrigo Suarez <ing.rsuarez@gmail.com>
2024-01-04 19:20:27 -0300

    livewire fix with session

--- config/session.php
+++ config/session.php
@@ -152,10 +152,11 @@ return [
     | Here you may change the domain of the cookie used to identify a session
     | in your application. This will determine which domains the cookie is
     | available to in your application. A sensible default has been set.
-    |
+    |'domain' => env('SESSION_DOMAIN'),
+    
     */
 
-    'domain' => env('SESSION_DOMAIN'),
+    'domain' => env('SESSION_DOMAIN', null),
 
     /*
     |--------------------------------------------------------------------------
@@ -168,7 +169,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    'secure' => env('SESSION_SECURE_COOKIE', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -178,7 +179,7 @@ return [
     | Setting this value to true will prevent JavaScript from accessing the
     | value of the cookie and the cookie will only be accessible through
     | the HTTP protocol. You are free to modify this option if needed.
-    |
+    |'secure' => env('SESSION_SECURE_COOKIE'), // in case of cookie
     */
 
     'http_only' => true,
