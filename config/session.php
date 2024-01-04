cd9add45efe0be1b4bd15d6583018236fe2d3249
Rodrigo Suarez <ing.rsuarez@gmail.com>
2024-01-04 20:10:42 -0300

    change session config

--- config/session.php
+++ config/session.php
@@ -152,11 +152,11 @@ return [
     | Here you may change the domain of the cookie used to identify a session
     | in your application. This will determine which domains the cookie is
     | available to in your application. A sensible default has been set.
-    |'domain' => env('SESSION_DOMAIN'),
+    |'domain' => env('SESSION_DOMAIN', null),
     
     */
 
-    'domain' => env('SESSION_DOMAIN', null),
+    'domain' => env('SESSION_DOMAIN'),
 
     /*
     |--------------------------------------------------------------------------
@@ -169,7 +169,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE'),
 
     /*
     |--------------------------------------------------------------------------
@@ -179,7 +179,7 @@ return [
     | Setting this value to true will prevent JavaScript from accessing the
     | value of the cookie and the cookie will only be accessible through
     | the HTTP protocol. You are free to modify this option if needed.
-    |'secure' => env('SESSION_SECURE_COOKIE'), // in case of cookie
+    |'secure' => env('SESSION_SECURE_COOKIE', false), // in case of cookie
     */
 
     'http_only' => true,
