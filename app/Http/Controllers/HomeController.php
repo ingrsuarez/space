201d51654d802787ac90aac5474abe9928d7a8c2
Rodrigo Suarez <ing.rsuarez@gmail.com>
2024-01-08 17:41:07 -0300

    Fix insurance by institution chart

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -137,14 +137,14 @@ class HomeController extends Controller
             'report_type' => 'group_by_relationship',
             'model' => 'App\Models\Appointment',
             'relationship_name' => 'insurance', // represents function user() on Transaction model
-            'group_by_field' => 'name', // users.name
-            'where_raw' => 'institution_id = '.$institution->id.' AND status != "cancelled"',
+            'group_by_field' => 'name', // insurance.name
+            'where_raw' => 'institution_id = '.$institution->id.' AND status != "cancelled" AND insurance_id != ""',
             // 'aggregate_function' => 'sum',
             // 'aggregate_field' => 'amount',
             // 'labels' => $labels,
             'chart_color' => '0,129,088',
             'filter_field' => 'created_at',
-            'filter_days' => 90, // show only transactions for last 30 days
+            'filter_days' => 90, // show only transactions for last 90 days
             
             // 'filter_period' => 'week', // show only transactions for this week
         ];
